51eee530774caeab490a71f90adbb11ba6781e8e
Bill Tran <bill@billjohntran.com>
2020-06-25 09:37:54 -0700

    add chart

--- client/pages/index.js
+++ client/pages/index.js
@@ -20,6 +20,57 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import {
+  Chart,
+  BarSeries,
+  Title,
+  ArgumentAxis,
+  ValueAxis,
+} from '@devexpress/dx-react-chart-material-ui';
+
+import { Animation } from '@devexpress/dx-react-chart';
+
+const data = [
+  { year: '1950', population: 2.525 },
+  { year: '1960', population: 3.018 },
+  { year: '1970', population: 3.682 },
+  { year: '1980', population: 4.440 },
+  { year: '1990', population: 5.310 },
+  { year: '2000', population: 6.127 },
+  { year: '2010', population: 6.930 },
+];
+
+class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data,
+    };
+  }
+
+  render() {
+    const { data: chartData } = this.state;
+
+    return (
+      <Paper>
+        <Chart
+          data={chartData}
+        >
+          <ArgumentAxis />
+          <ValueAxis max={7} />
+
+          <BarSeries
+            valueField="population"
+            argumentField="year"
+          />
+          <Title text="World population" />
+          <Animation />
+        </Chart>
+      </Paper>
+    );
+  }
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -153,6 +204,9 @@ export default function Index() {
             </TableBody>
           </Table>
         </TableContainer>
+      </Container>
+      <Container>
+        <Demo />
       </Container>
     </Container>
   );
